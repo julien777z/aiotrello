b9bd64efcde3508d61b0a93c9ced6b8aa5a698c3
zomien <xomien01@gmail.com>
2021-11-26 00:00:48 -0800

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name="aiotrello",
-	version="0.0.5",
+	version="0.0.7.7",
 	description="Async Trello library",
 	url="https://github.com/zomien/aiotrello",
 	keywords=["async", "trello"],
